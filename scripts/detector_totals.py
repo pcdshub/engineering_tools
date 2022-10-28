675556ff46512a35a16d603be5e924a7fb63b814
Murali Shankar <mshankar@slac.stanford.edu>
2022-10-27 21:34:38 -0700

    Confirm to PEP8

--- scripts/detector_totals.py
+++ scripts/detector_totals.py
@@ -3,12 +3,9 @@
 Script for the detector group to pull totals out of the elog.
 We get DAQ run parameters for all experiments and count them up.
 """
-import os
 import sys
-import json
 import logging
 import argparse
-from datetime import datetime
 from collections import OrderedDict
 import requests
 import pytz
@@ -19,20 +16,30 @@ from krtc import KerberosTicket
 logger = logging.getLogger(__name__)
 krbheaders = KerberosTicket("HTTP@pswww.slac.stanford.edu").getAuthHeaders()
 tz = pytz.timezone('America/Los_Angeles')
+lgbkprefix = "https://pswww.slac.stanford.edu/ws-kerb/lgbk/lgbk/"
+
 
 def getDAQDetectorTotals(experiment):
     """Returns a total number of events per detector for this experiment"""
     logger.info(f"Getting DAQ Detector totals for {experiment}")
     ret = OrderedDict()
 
-    resp = requests.get(f"https://pswww.slac.stanford.edu/ws-kerb/lgbk//lgbk/{experiment}/ws/run_table_sources", headers=krbheaders).json()
+    resp = requests.get(
+        f"{lgbkprefix}/{experiment}/ws/run_table_sources",
+        headers=krbheaders).json()
     rps = resp["value"].get("DAQ", [])
-    daqdets = map(lambda x: x["source"].replace("params.", ""), filter(lambda x: x["label"].startswith("DAQ Detectors/"), rps))
-    rpvals = requests.get(f"https://pswww.slac.stanford.edu/ws-kerb/lgbk//lgbk/{experiment}/ws/get_run_params_for_all_runs",
-        params={"param_names": ",".join(list(daqdets) + [ "DAQ Detector Totals/Events" ])}, headers=krbheaders).json()["value"]
+    daqdets = map(lambda x: x["source"].replace("params.", ""),
+                  filter(lambda x: x["label"].startswith("DAQ Detectors/"),
+                         rps))
+    detevnms = ",".join(list(daqdets) + ["DAQ Detector Totals/Events"])
+    rpvals = requests.get(
+        f"{lgbkprefix}/{experiment}/ws/get_run_params_for_all_runs",
+        params={"param_names": detevnms},
+        headers=krbheaders).json()["value"]
     for run in rpvals:
         eventcount = run["params"].get("DAQ Detector Totals/Events", None)
-        dets_in_run = filter(lambda x: x.startswith("DAQ Detectors/"), run["params"].keys())
+        dets_in_run = filter(lambda x: x.startswith("DAQ Detectors/"),
+                             run["params"].keys())
         if eventcount and dets_in_run:
             for detector in dets_in_run:
                 if detector not in ret:
@@ -40,38 +47,57 @@ def getDAQDetectorTotals(experiment):
                 ret[detector] = ret[detector] + int(eventcount)
     return ret
 
+
 def getExperiments(run_period, after, before):
     """ Return all the experiments in the specified run period.
     Also, if start and end are specified, include other experiments as follows
-    after - If not None, also include other experiments except those whose last run is before the specified time
-    before - If not None, also include other experiments except those whose first run is after the specified time
+    after - If not None, also include other experiments
+        except those whose last run is before the specified time
+    before - If not None, also include other experiments
+        except those whose first run is after the specified time
     """
-    resp = requests.get("https://pswww.slac.stanford.edu/ws-kerb/lgbk/lgbk/ws/experiments", params={"categorize": "instrument_runperiod", "sortby" : "name"}, headers=krbheaders).json()
+    resp = requests.get(
+                f"{lgbkprefix}/ws/experiments",
+                params={"categorize": "instrument_runperiod",
+                        "sortby": "name"},
+                headers=krbheaders).json()
     exps = []
-    for k,v in resp["value"].items():
+    for k, v in resp["value"].items():
         insexps = map(lambda x: x["_id"], v.get("Run " + str(run_period), []))
         exps.extend(insexps)
 
     def last_run_exists(exp):
         return exp.get("last_run", {}).get("begin_time", None)
+
     def first_run_exists(exp):
         return exp.get("first_run", {}).get("begin_time", None)
+
     def last_run_before_specified_after(exp):
-        return exp.get("last_run", {}).get("begin_time", None) and dateparser.parse(exp["last_run"]["begin_time"]).astimezone(tz) < after
+        return exp.get("last_run", {}).get("begin_time", None) \
+                and dateparser.parse(exp["last_run"]["begin_time"])\
+                .astimezone(tz) < after
+
     def first_run_after_specified_before(exp):
-        return exp.get("first_run", {}).get("begin_time", None) and dateparser.parse(exp["first_run"]["begin_time"]).astimezone(tz) > before
+        return exp.get("first_run", {}).get("begin_time", None) \
+                and dateparser.parse(exp["first_run"]["begin_time"])\
+                .astimezone(tz) > before
 
     sef = None
     if after and before:
-        sef = lambda x: last_run_exists(x) and not last_run_before_specified_after(x) and first_run_exists(x) and not first_run_after_specified_before(x)
+        sef = lambda x: last_run_exists(x) \
+                and not last_run_before_specified_after(x) \
+                and first_run_exists(x) \
+                and not first_run_after_specified_before(x)
     elif after:
-        sef = lambda x: last_run_exists(x) and not last_run_before_specified_after(x)
+        sef = lambda x: last_run_exists(x) \
+                and not last_run_before_specified_after(x)
     elif before:
-        sef = lambda x: first_run_exists(x) and not first_run_after_specified_before(x)
+        sef = lambda x: first_run_exists(x) \
+                and not first_run_after_specified_before(x)
 
     if sef:
         expsset = set(exps)
-        for ins,rps in resp["value"].items():
+        for ins, rps in resp["value"].items():
             for rp, el in rps.items():
                 for e in el:
                     if sef(e):
@@ -80,18 +106,39 @@ def getExperiments(run_period, after, before):
 
     return exps
 
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("-v", "--verbose",    action='store_true', help="Turn on verbose logging")
-    parser.add_argument("--run_period", action='store', help="The last two digits of the run period, for example 18", required=True, type=int)
-    parser.add_argument("--show_matched_experiments_only", action='store_true', help="Only show the matching experiment names; do not actually run the report. Used for debugging.")
-    parser.add_argument("--after", help="Include other experiments except those whose last run is before the specified time")
-    parser.add_argument("--before", help="Include other experiments except those whose first run is after the specified time")
+    parser.add_argument("-v",
+                        "--verbose",
+                        action='store_true',
+                        help="Turn on verbose logging")
+    parser.add_argument("--run_period",
+                        action='store',
+                        help="The last two digits of the run period, "
+                             "for example 18",
+                        required=True,
+                        type=int)
+    parser.add_argument("--show_matched_experiments_only",
+                        action='store_true',
+                        help="Only show the matching experiment names;"
+                             " do not actually run the report. "
+                             "Used for debugging.")
+    parser.add_argument("--after",
+                        help="Include other experiments except those "
+                        "whose last run is before the specified time")
+    parser.add_argument("--before",
+                        help="Include other experiments except those "
+                        "whose first run is after the specified time")
 
     args = parser.parse_args()
     logging.basicConfig(level=logging.DEBUG if args.verbose else logging.INFO)
-    after = dateparser.parse(args.after).astimezone(tz) if args.after else None
-    before = dateparser.parse(args.before).astimezone(tz) if args.before else None
+
+    def dpp(x):
+        dateparser.parse(x).astimezone(tz) if x else None
+
+    after = dpp(args.after)
+    before = dpp(args.before)
 
     experiments = getExperiments(args.run_period, after, before)
     logger.debug(experiments)
@@ -116,4 +163,5 @@ if __name__ == '__main__':
 
     print("Totals across all experiments")
     for detector, count in det_counts.items():
-        print("{0: <32} --> {1}".format(detector.replace("DAQ Detectors/", ""), count))
+        print("{0: <32} --> {1}"
+              .format(detector.replace("DAQ Detectors/", ""), count))
