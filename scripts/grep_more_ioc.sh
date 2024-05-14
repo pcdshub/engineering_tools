655c7b98828f8cd92906f5a99b8cbff8294e3cf2
aberges-SLAC <aberges@slac.stanford.edu>
2024-05-14 11:46:46 -0700

    MNT: Proper quoting for  in grep_more_ioc.sh

--- scripts/grep_more_ioc.sh
+++ scripts/grep_more_ioc.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
 # execute python script
-/cds/group/pcds/pyps/conda/py39/envs/pcds-5.8.4/bin/python $ENG_TOOLS_SCRIPTS/grep_more_ioc.py "$@"
+/cds/group/pcds/pyps/conda/py39/envs/pcds-5.8.4/bin/python "$ENG_TOOLS_SCRIPTS/grep_more_ioc.py" "$@"
