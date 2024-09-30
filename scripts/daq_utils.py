0da431b11bdcb77cb9a96814e36b6a306c1a5a34
monarin <monarin@gmail.com>
2024-09-30 15:58:49 -0700

    formatting and use argparse

--- scripts/daq_utils.py
+++ scripts/daq_utils.py
@@ -10,13 +10,14 @@ import os, sys
 import getpass
 import time
 
-DAQBATCH_HUTCHES=['tmo', 'rix']
+DAQBATCH_HUTCHES = ["tmo", "rix"]
 LOCALHOST = socket.gethostname()
-SLURM_PARTITION='drpq'
-SLURM_JOBNAME='submit_daq'
-DAQBATCH_SCRIPT='submit_daq.sh'
-DAQBATCH_OUTPUT='slurm_daq.log'
-MAX_RETRIES=10
+SLURM_PARTITION = "drpq"
+SLURM_JOBNAME = "submit_daq"
+DAQBATCH_SCRIPT = "submit_daq.sh"
+DAQBATCH_OUTPUT = "slurm_daq.log"
+MAX_RETRIES = 10
+
 
 def silentremove(filename):
     try:
@@ -25,8 +26,10 @@ def silentremove(filename):
         if e.errno != errno.ENOENT:  # errno.ENOENT = no such file or directory
             raise
 
+
 def call_subprocess(*args):
     return subprocess.check_output(args, stderr=PIPE, universal_newlines=True)
+
 
 def call_sbatch(cmd, nodelist, scripts_dir):
     sb_script = "#!/bin/bash\n"
@@ -42,20 +45,20 @@ def call_sbatch(cmd, nodelist, scripts_dir):
     daqbatch_script = os.path.join(scripts_dir, DAQBATCH_SCRIPT)
     with open(daqbatch_script, "w") as f:
         f.write(sb_script)
-    call_subprocess('sbatch', daqbatch_script)
+    call_subprocess("sbatch", daqbatch_script)
     silentremove(daqbatch_script)
+
 
 class SbatchManager:
     def __init__(self, user):
         self.user = user
 
-
     def get_job_info(self):
         # Use squeue to get JobId, Comment, JobName, Status, and Reasons (Node List)
         format_string = '"%i %k %j %T %R"'
         lines = call_subprocess(
-                    "squeue", "-u", self.user, "-h", "-o", format_string
-                ).splitlines()
+            "squeue", "-u", self.user, "-h", "-o", format_string
+        ).splitlines()
         job_details = {}
         for i, job_info in enumerate(lines):
             cols = job_info.strip('"').split()
@@ -89,33 +92,31 @@ class SbatchManager:
 
 
 class DaqManager:
-    def __init__(self, opts):
-        self.opts = opts
-        self.verbose = False
-        for o, a in self.opts:
-            if o in ('-v', '--verbose'):
-                self.verbose = True
+    def __init__(self, verbose=False):
+        self.verbose = verbose
         self.hutch = call_subprocess("get_info", "--gethutch")
-        self.user = self.hutch+'opr'
+        self.user = self.hutch + "opr"
         self.sbman = SbatchManager(self.user)
-        self.scripts_dir = f'/reg/g/pcds/dist/pds/{self.hutch}/scripts'
+        self.scripts_dir = f"/reg/g/pcds/dist/pds/{self.hutch}/scripts"
         self.set_cnf_file_and_platform()
 
     def isdaqbatch(self, quiet=False):
         if self.hutch in DAQBATCH_HUTCHES:
-            if not quiet: print('true')
+            if not quiet:
+                print("true")
             return True
         else:
-            if not quiet: print('false')
+            if not quiet:
+                print("false")
             return False
 
     def isvaliduser(self):
-        if getpass.getuser().find('opr') > -1:
+        if getpass.getuser() == self.user:
             return True
         return False
 
     def waitfor(self, subcmd):
-        if subcmd == 'stop':
+        if subcmd == "stop":
             daq_host = self.wheredaq(quiet=True)
             if daq_host is not None:
                 if self.isdaqbatch(quiet=True):
@@ -123,52 +124,57 @@ class DaqManager:
                         daq_host = self.wheredaq(quiet=True)
                         if daq_host is None:
                             break
-                        print(f'wait for daqbatch to stop #retry: {i_retry}')
+                        print(f"wait for daqbatch to stop #retry: {i_retry}")
                         time.sleep(1)
                 else:
-                    print(f'the DAQ did not stop properly (DAQ HOST: {daq_host}), exit now and try again or call your POC or the DAQ phone')
+                    print(
+                        f"the DAQ did not stop properly (DAQ HOST: {daq_host}), exit now and try again or call your POC or the DAQ phone"
+                    )
 
     def set_cnf_file_and_platform(self):
-        self.cnf_file = f'{self.hutch}.py'
-        
-        cc = {'platform': None}
+        self.cnf_file = f"{self.hutch}.py"
+
+        cc = {"platform": None}
         configfilename = os.path.join(self.scripts_dir, self.cnf_file)
         try:
-            exec(compile(open(configfilename).read(), configfilename, 'exec'), {}, cc)
-            if type(cc['platform']) == type('') and cc['platform'].isdigit():
-                rv = int(cc['platform'])
+            exec(compile(open(configfilename).read(), configfilename, "exec"), {}, cc)
+            if type(cc["platform"]) == type("") and cc["platform"].isdigit():
+                rv = int(cc["platform"])
         except:
-            print('deduce_platform Error:', sys.exc_info()[1])
+            print("deduce_platform Error:", sys.exc_info()[1])
             raise
         self.platform = rv
 
     def wheredaq(self, quiet=False):
-        """ Locate where the daq is running.
-        
+        """Locate where the daq is running.
+
         For daqbatch, we use slurm to check if the hutch user is running control_gui.
         """
         daq_host = None
         # Use control_gui job name to locate the running host for the daq
         job_details = self.sbman.get_job_info()
-        if 'control_gui' in job_details:
-            daq_host = job_details['control_gui']['nodelist']
-        
+        if "control_gui" in job_details:
+            daq_host = job_details["control_gui"]["nodelist"]
+
         if not quiet:
-            if daq_host is None: 
-                print(f'DAQ is not running in {self.hutch}')
+            if daq_host is None:
+                print(f"DAQ is not running in {self.hutch}")
             else:
-                print(f'DAQ is running on {daq_host}')
+                print(f"DAQ is running on {daq_host}")
         return daq_host
-    
+
     def calldaq(self, subcmd, daq_host=None):
-        prog = 'daqbatch'
-        cmd = f'pushd {self.scripts_dir}'+' > /dev/null;'
-        cmd += f'source {os.path.join(self.scripts_dir, "setup_env.sh")}'+';'
-        cmd += f'WHEREPROG=$(which {prog}); set -x; $WHEREPROG {subcmd} {os.path.join(self.scripts_dir,self.cnf_file)}'+'; { set +x; } 2>/dev/null;'
-        cmd += f'popd'+' > /dev/null;'
-        
-        if subcmd == 'restart':
-            query_daq_host = self.wheredaq() 
+        prog = "daqbatch"
+        cmd = f"pushd {self.scripts_dir}" + " > /dev/null;"
+        cmd += f'source {os.path.join(self.scripts_dir, "setup_env.sh")}' + ";"
+        cmd += (
+            f"WHEREPROG=$(which {prog}); set -x; $WHEREPROG {subcmd} {os.path.join(self.scripts_dir,self.cnf_file)}"
+            + "; { set +x; } 2>/dev/null;"
+        )
+        cmd += f"popd" + " > /dev/null;"
+
+        if subcmd == "restart":
+            query_daq_host = self.wheredaq()
         else:
             query_daq_host = self.wheredaq(quiet=True)
 
@@ -182,41 +188,21 @@ class DaqManager:
             print(ret.stdout.decode())
         else:
             call_sbatch(cmd, daq_host, self.scripts_dir)
-        
+
         self.waitfor(subcmd)
 
-
     def stopdaq(self):
-        """ Stop the running daq for the current user.
+        """Stop the running daq for the current user.
         Note that daq host is determined by .running file or squeue (daqbatch).
         """
-        self.calldaq('stop')
+        self.calldaq("stop")
 
-    def startdaq(self, daq_host):
-        self.calldaq('start', daq_host=daq_host)
-
-    def restart_daqbatch(self, daq_host):
-        st = time.monotonic()
-        self.calldaq('restart', daq_host=daq_host)
-        en = time.monotonic()
-        print(f'took {en-st:.4f}s. for starting the DAQ')
-
-    def restartdaq(self):
+    def restartdaq(self, daq_host):
         if not self.isvaliduser():
-            print(f'Please run the DAQ from the operator account!')
+            print(f"Please run the DAQ from the operator account!")
             return
-	
-        daq_host = LOCALHOST
-        # User can use -m to specify the host to run the daq
-        for o, a in self.opts:
-            if o == '-m':
-                daq_host = a
-                break
-	
-        if self.isdaqbatch(quiet=True):
-            self.restart_daqbatch(daq_host)
-        else:
-            self.stopdaq()
-            self.startdaq(daq_host)
 
-        
+        st = time.monotonic()
+        self.calldaq("restart", daq_host=daq_host)
+        en = time.monotonic()
+        print(f"took {en-st:.4f}s. for starting the DAQ")
--- scripts/run_daq_utils.py
+++ scripts/run_daq_utils.py
@@ -1,27 +1,58 @@
-import os,sys,time
-from daq_utils import DaqManager
-import getopt
+#!/usr/bin/env python3
+"""
+Enforce more checks and controls for running daqbatch.
+"""
 
-help_messages = {
-        'wheredaq': 'Discover what host is running the daq in the current hutch, if any.',
-        'stopdaq': 'Stop the daq in the current hutch.',
-        'restartdaq': 'Verify requirements for running the daq then stop and start it.',
-        'isdaqbatch': "Determine if the current user's hutch uses daqbatch",
-        }
+from daq_utils import DaqManager, LOCALHOST
+import argparse
+
+
+def restartdaq(daqmgr, args):
+    daqmgr.restartdaq(args.aimhost)
+
+
+def wheredaq(daqmgr, args):
+    daqmgr.wheredaq()
+
+
+def stopdaq(daqmgr, args):
+    daqmgr.stopdaq()
+
+
+def isdaqbatch(daqmgr, args):
+    daqmgr.isdaqbatch()
+
 
 if __name__ == "__main__":
-    commands = ['wheredaq', 'stopdaq', 'restartdaq', 'isdaqbatch']
-    opts, args = getopt.getopt(sys.argv[2:], 'hm:dv', ["help","dss"])
-    cmd = sys.argv[1]
-    if cmd not in commands:
-        print(f'Unsupported command {cmd}')
-        raise
-    for o, a in opts:
-        if o in ("-h", "--help"):
-            print(help_messages[cmd])
-            exit()
+    parser = argparse.ArgumentParser(prog="run_daq_utils", description=__doc__)
 
-    daqman = DaqManager(opts)
-    func = getattr(daqman, cmd)
-    result = func()
+    parser.add_argument("-v", "--verbose", action="store_false")
 
+    subparsers = parser.add_subparsers()
+    psr_restart = subparsers.add_parser(
+        "restartdaq",
+        help="Verify requirements for running the daq then stop and start it.",
+    )
+    psr_restart.add_argument("-m", "--aimhost", action="store_const", const=LOCALHOST)
+    psr_restart.set_defaults(func=restartdaq)
+
+    psr_where = subparsers.add_parser(
+        "wheredaq",
+        help="Discover what host is running the daq in the current hutch, if any.",
+    )
+    psr_where.set_defaults(func=wheredaq)
+
+    psr_stop = subparsers.add_parser(
+        "stopdaq", help="Stop the daq in the current hutch."
+    )
+    psr_stop.set_defaults(func=stopdaq)
+
+    psr_isdaqbatch = subparsers.add_parser(
+        "isdaqbatch", help="Determine if the current hutch uses daqbatch"
+    )
+    psr_isdaqbatch.set_defaults(func=isdaqbatch)
+
+    args = parser.parse_args()
+
+    daqmgr = DaqManager(args.verbose)
+    args.func(daqmgr, args)
