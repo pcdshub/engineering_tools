85ca2fe58040bdb17a65134bd34adfdd2dc71d2c
monarin <monarin@gmail.com>
2024-09-30 17:13:09 -0700

    flag a problem for future fix

--- scripts/daq_utils.py
+++ scripts/daq_utils.py
@@ -28,6 +28,9 @@ def silentremove(filename):
 
 
 def call_subprocess(*args):
+    # FIXME: potentialy wanted to replace with 
+    # return subprocess.check_output(args, stderr=PIPE, universal_newlines=True)
+    # current block: return values are not in expected format. 
     cc = subprocess.run(args, stdout=PIPE, stderr=PIPE)
     output = None
     if not cc.returncode:
