c166007404a513501d6fd02d24ec43ddb96658c0
Vincent Esposito <espov@slac.stanford.edu>
2026-01-20 17:07:23 -0800

    XPP is now a daqmgr hutch

--- scripts/daq_utils.py
+++ scripts/daq_utils.py
@@ -11,7 +11,7 @@ import subprocess
 import time
 from subprocess import PIPE
 
-DAQMGR_HUTCHES = ["tmo", "rix", "txi", "ued", "mfx"]
+DAQMGR_HUTCHES = ["tmo", "rix", "txi", "xpp", "ued", "mfx"]
 LOCALHOST = socket.gethostname()
 SLURM_PARTITION = "drpq"
 SLURM_JOBNAME = "submit_daq"
